8b7af20bcdfa1a0e429770dbdc556cc77a35637b
pvtbx <tippspd@gmail.com>
2023-06-15 15:34:37 -0500

    cleaned up formatting

--- sn_bentley_integration.js
+++ sn_bentley_integration.js
@@ -7,10 +7,10 @@ let stagingCommunityInstanceAPI = 'https://stage-communities-bentley2-com.tellig
 console.log(`Staging community Base64 Token: ${base64Token} `);
 
 // variables for connecting to Bentley ServiceNow Dev Instance
-let serviceNowInstance = 'https://bentleysystemsdev.service-now.com'; 
+let serviceNowInstance = 'https://bentleysystemsdev.service-now.com';
 let serviceNowTable = 'sn_communities_forum';
 let serviceNowUser = 'pat.tipps'; // must be an account with 'Web services' checked - can't be admin account
-let serviceNowPass = 'aJjC)B5>jY9p2yJe]}wt6z2=V5rrC>RAx4=jcA5D'; 
+let serviceNowPass = 'aJjC)B5>jY9p2yJe]}wt6z2=V5rrC>RAx4=jcA5D';
 let serviceNowUserAuth = Buffer.from(serviceNowUser + ':' + serviceNowPass).toString('base64'); // authorization must be a concatentation of servicenow username and password converted to base64
 console.log(`Bentley Dev Base64 Token: ${base64Token} `);
 
@@ -20,35 +20,36 @@ fetch(`${stagingCommunityInstanceAPI}`, { // make API call to staging community 
     'Rest-User-Token': base64Token // Rest-User-Token MUST be passed as authorization
   }
 })
-.then(response => {
-  if (!response.ok) { // if there's no response, return an error
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return response.json(); // if the response is OK, this returns a promise
-})
-.then(data => {
+  .then(response => {
+    if (!response.ok) { // if there's no response, return an error
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json(); // if the response is OK, this returns a promise
+  })
+  .then(data => {
 
-  // Gather the names of all Forums
-  for(let i = 0; i < data.Forums.length; i++){
-    console.log(data.Forums[i].Name + " " + data.Forums[i].Id); // log the data received
-  let recordData = {
-    "name": data.Forums[i].Name,
-    "description" : data.Forums[i].Description
-  };
-  fetch(`${serviceNowInstance}/api/now/table/${serviceNowTable}`, {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Basic ' + serviceNowUserAuth,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(recordData)
+    // Gather the names of all Forums
+    for (let i = 0; i < data.Forums.length; i++) {
+      console.log(data.Forums[i].Name + " " + data.Forums[i].Id); // log the data received
+      let recordData = { // data that will be appended to new record
+        "name": data.Forums[i].Name,
+        "description": data.Forums[i].Description
+      };
+      fetch(`${serviceNowInstance}/api/now/table/${serviceNowTable}`, { // make an call to the Table API
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic ' + serviceNowUserAuth,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(recordData)
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error => {
+          console.log('There was a problem with the fetch operation [Dev Instance]: ' + error.message)
+        })
+    }
   })
-  .then(response => response.json())
-  .then(json => console.log(json))
   .catch(error => {
-    console.log('There was a problem with the fetch operation [Dev Instance]: ' + error.message)
-  })
-  }})
-.catch(error => {
-  console.log('There was a problem with the fetch operation [Staging Community]: ' + error.message);
-});
+    console.log('There was a problem with the fetch operation [Staging Community]: ' + error.message);
+  });
