5e518e9f5dcd587151729dfb5ff40b5f76ea234b
pvtbx <tippspd@gmail.com>
2023-06-20 16:24:20 -0500

    massive updates

--- components/createQuestionsGC.js
+++ components/createQuestionsGC.js
@@ -1,54 +1,89 @@
-let apiKey = 'cmak_2445_ILoBJinETRnWyKuzCjmDjQpAplziXpOYkEjUqehffds'; // staging community api key
-let username = '6dc09cb5-5442-47e2-a67d-088c2a11b8a5'; // staging community profile username
-let token = `${apiKey}:${username}`; // authorization for staging site is a concatentation of the api key and the username
-let base64Token = Buffer.from(token).toString('base64'); // authorization must be converted to base64 to be passed to staging community instance
-// 360 = Generative Components Threads
-let listForumsEndpoint = "api.ashx/v2/forums/360/threads.json"; // List Form Thread REST Endpoint
+import { DOMParser, XMLSerializer } from 'xmldom';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+
+
+let apiKey = 'cmak_2445_ILoBJinETRnWyKuzCjmDjQpAplziXpOYkEjUqehffds';
+let username = '6dc09cb5-5442-47e2-a67d-088c2a11b8a5';
+let token = `${apiKey}:${username}`;
+let base64Token = Buffer.from(token).toString('base64');
+let listForumsEndpoint = "api.ashx/v2/forums/360/threads.json";
 let stagingCommunityInstanceAPI = `https://stage-communities-bentley2-com.telligenthosting.net/${listForumsEndpoint}`;
 
-let serviceNowUser = 'pat.tipps'; // must be an account with 'Web services' checked - can't be admin account
+let serviceNowUser = 'pat.tipps';
 let serviceNowPass = 'aJjC)B5>jY9p2yJe]}wt6z2=V5rrC>RAx4=jcA5D';
-let serviceNowUserAuth = Buffer.from(serviceNowUser + ':' + serviceNowPass).toString('base64'); // authorization must be a concatentation of servicenow username and password converted to base64
+let serviceNowUserAuth = Buffer.from(serviceNowUser + ':' + serviceNowPass).toString('base64');
 
-export function createGenerativeComponentQuestions() {
-    fetch(`${stagingCommunityInstanceAPI}`, { // make API call to staging community instance
-        method: 'GET',
-        headers: {
-            'Rest-User-Token': base64Token // Rest-User-Token MUST be passed as authorization
+const downloadDirectory = './downloads'; // Define the directory where images will be downloaded
+
+export async function createGenerativeComponentQuestions() {
+    try {
+        const response = await axios.get(stagingCommunityInstanceAPI, {
+            headers: {
+                'Rest-User-Token': base64Token
+            }
+        });
+
+        const threads = response.data.Threads;
+
+        for (let i = 0; i < threads.length; i++) {
+            let bodyContent = threads[i].Body;
+
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(bodyContent, 'text/html');
+            const imgTags = doc.getElementsByTagName('img');
+
+            let downloadPromises = [];
+            for (let j = 0; j < imgTags.length; j++) {
+                const img = imgTags[j];
+                const oldUrl = img.getAttribute('src');
+                const downloadPromise = downloadImage(oldUrl).then(newUrl => {
+                    img.setAttribute('src', newUrl);
+                });
+                downloadPromises.push(downloadPromise);
+            }
+
+            await Promise.all(downloadPromises);
+
+            const serializer = new XMLSerializer();
+            const updatedBodyContent = serializer.serializeToString(doc);
+
+            let threadRecord = {
+                "question": threads[i].Subject + " [Generative Components Forum]",
+                "question_details": updatedBodyContent,
+                "kb_knowledge_base": "Generative Components"
+            };
+
+            await axios.post('https://bentleysystemsdev.service-now.com/api/now/table/kb_social_qa_question', threadRecord, {
+                headers: {
+                    'Authorization': 'Basic ' + serviceNowUserAuth,
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            console.log(`Thread ${threads[i].Subject} has been successfully migrated.`);
         }
-    })
-        .then(response => {
-            if (!response.ok) { // if there's no response, return an error
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json(); // if the response is OK, this returns a promise
-        })
-        .then(data => {
-            // This lists out all of the threads that belong to Generative Component Forum
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 
-            for(let i = 0; i < data.Threads.length; i++){
-                let threadRecord = {
-                    "question": data.Threads[i].Subject + " [Generative Components Forum]",
-                    "question_details": data.Threads[i].Body,
-                    "kb_knowledge_base" : "Generative Components"
-                };
+async function downloadImage(url) {
+    const fileName = path.basename(new URL(url).pathname);
+    const localPath = path.join(downloadDirectory, fileName);
 
-                fetch('https://bentleysystemsdev.service-now.com/api/now/table/kb_social_qa_question',{
-                    method: 'POST',
-                    headers: {
-                        'Authorization':'Basic ' + serviceNowUserAuth,
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(threadRecord)
-                })
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                })
-                .catch(error => console.log(error.message));
-            }
-        })
-        .catch(error => console.log(error.message))
+    const writer = fs.createWriteStream(localPath);
+
+    return new Promise((resolve, reject) => {
+        https.get(url, (response) => {
+            response.pipe(writer);
+
+            writer.on('finish', () => resolve(localPath));
+            writer.on('error', reject);
+        });
+    });
 }
 
 createGenerativeComponentQuestions();
